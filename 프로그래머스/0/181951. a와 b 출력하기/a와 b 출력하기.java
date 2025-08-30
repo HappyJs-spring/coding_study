ddf1d4dfc07c6c9d2ef4eb5368b076dbb540f18d
doseong :) <118369702+HappyJs-spring@users.noreply.github.com>
2025-08-30 22:56:02 +0900

    [level 0] Title: a와 b 출력하기, Time: 205.48 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // nextInt(): 공백(스페이스, 탭, 엔터 등)을 기준으로 정수 하나를 읽는다
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+		System.out.println("a = " + a + "\nb = " + b);
+    }
+}
